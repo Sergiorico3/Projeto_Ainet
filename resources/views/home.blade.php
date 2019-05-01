e43a2c678127b9950c78e69d4e23c29ab85938a7
sergiorico3 <clubofsk8ers@hotmail.com>
2019-05-01 11:20:57 +0100

    criação da master blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,107 +1,21 @@
-<!doctype html>
-<html lang="en">
-    <head>
-        <title>FlightClub</title>
-        <!-- Required meta tags -->
-        <meta charset="utf-8">
-        <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1, shrink-to-fit=no">
+@extends('layouts.master') @section('title', 'FlightClub') @section('content')
+<body>
+   
+    <header>
 
-        <!-- Bootstrap CSS -->
-        <link
-            rel="stylesheet"
-            href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-            integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-            crossorigin="anonymous">
-    </head>
-    <body>
-        <!-- Navigation -->
-        <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-            <div class="container">
-                <a class="navbar-brand" href="#">FlightClub</a>
-                <button
-                    class="navbar-toggler"
-                    type="button"
-                    data-toggle="collapse"
-                    data-target="#navbarResponsive"
-                    aria-controls="navbarResponsive"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarResponsive">
-                    <ul class="navbar-nav ml-auto">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="#">Home
-                                <span class="sr-only">(current)</span>
-                            </a>
-                        </li>
-                        <!-- Adicionar mais tarde mais links <li class="nav-item"> <a class="nav-link"
-                        href="#">About</a> </li> <li class="nav-item"> <a class="nav-link"
-                        href="#">Services</a> </li> -->
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ url('/login') }}">Login</a>
-                        </li>
-                    </ul>
+        <div class="carousel-inner" role="listbox">
+            <!-- Slide One - Set the background image for this slide in the line below -->
+            <div
+                class="carousel-item active"
+                style="background-image: url('https://www.publicdomainpictures.net/pictures/260000/velka/paper-plane.jpg')">
+                <div class="carousel-caption d-none d-md-block">
+                    <h2 class="display-4"><i class="fab fa-telegram-plane"></i> FlightClub</h2>
+                    <p class="lead">Web app para gestão do Aeroclube FlightClub.</p>
                 </div>
             </div>
-        </nav>
 
-        <header>
-            
-                <div class="carousel-inner" role="listbox">
-                    <!-- Slide One - Set the background image for this slide in the line below -->
-                    <div
-                        class="carousel-item active"
-                        style="background-image: url('https://www.publicdomainpictures.net/pictures/260000/velka/paper-plane.jpg')">
-                        <div class="carousel-caption d-none d-md-block">
-                            <h2 class="display-4">FlightClub</h2>
-                            <p class="lead">Web app para gestão do Aeroclube FlightClub.</p>
-                        </div>
-                    </div>
+        </div>
 
-                </div>
-                
-    
-        </header>
+    </header>
 
-        <!-- Page Content -->
-        <section class="py-5">
-            <div class="container">
-                <h1 class="display-4">Full Page Image Slider</h1>
-                <p class="lead">The background images for the slider are set directly in the
-                    HTML using inline CSS. The images in this snippet are from
-                    <a href="https://unsplash.com">Unsplash</a>, taken by
-                    <a href="https://unsplash.com/@joannakosinska">Joanna Kosinska</a>!</p>
-            </div>
-        </section>
-
-        <!-- Optional JavaScript -->
-        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-        <script
-            src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-            crossorigin="anonymous"></script>
-        <script
-            src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-            integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
-            crossorigin="anonymous"></script>
-        <script
-            src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-            integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-            crossorigin="anonymous"></script>
-    </body>
-
-    <style>
-        .carousel-item {
-            height: 100vh;
-            min-height: 350px;
-            background: no-repeat center center scroll;
-            -webkit-background-size: cover;
-            -moz-background-size: cover;
-            -o-background-size: cover;
-            background-size: cover;
-        }
-    </style>
-</html>
+    @endsection
