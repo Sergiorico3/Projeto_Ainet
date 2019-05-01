13d70e3f58daf062a0525773dcd05fd593d995fd
sergiorico3 <clubofsk8ers@hotmail.com>
2019-05-01 15:31:41 +0100

    list user

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -26,6 +26,36 @@
                         </div>
 
                         <div class="form-group row">
+                            <label for="nome_informal" class="col-md-4 col-form-label text-md-right">{{ __('Nome Informal') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="nome_informal" type="text" class="form-control{{ $errors->has('nome_informal') ? ' is-invalid' : '' }}" name="nome_informal" value="{{ old('nome_informal') }}" required autofocus>
+
+                                @if ($errors->has('nome_informal'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('nome_informal') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="sexo" class="col-md-4 col-form-label text-md-right">{{ __('Sexo') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="sexo" type="text" class="form-control{{ $errors->has('sexo') ? ' is-invalid' : '' }}" name="sexo" value="{{ old('sexo') }}" required autofocus>
+
+                                @if ($errors->has('sexo'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('sexo') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+                        
+                        
+
+                        <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
                             <div class="col-md-6">
@@ -58,6 +88,20 @@
 
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                            </div>
+                        </div>
+                        
+                        <div class="form-group row">
+                            <label for="num_socio" class="col-md-4 col-form-label text-md-right">{{ __('Num Socio') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="num_socio" type="text" class="form-control{{ $errors->has('num_socio') ? ' is-invalid' : '' }}" name="num_socio" value="{{ old('num_socio') }}" required autofocus>
+
+                                @if ($errors->has('num_socio'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('num_socio') }}</strong>
+                                    </span>
+                                @endif
                             </div>
                         </div>
 
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,4 @@
-@extends('layouts.app')
-
-@section('content')
+@extends('layouts.app') @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -9,15 +7,36 @@
 
                 <div class="card-body">
                     @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
+                    @endif You are logged in!
 
-                    You are logged in!
+                </div>
+                
+                <form action="{{ action('UserController@index', $user->id) }}" method="post">
+
+                <div class="card-body">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Email</th>
+                                <th>Fullname</th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                            <tr>
+                                <td>{{ $user->email }}
+                                </td>
+                                <td>{{ $user->name }}
+                                </td>
+                            </tr>
+                        </tbody>
+
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-@endsection
+    @endsection
