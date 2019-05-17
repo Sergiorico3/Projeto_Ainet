e3f672c058fe971e73b1b17213a09967408697cf
sergiorico3 <clubofsk8ers@hotmail.com>
2019-05-17 10:10:13 +0100

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -74,7 +74,6 @@
                                 <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                             </li>
                             @endif @else
-
                             
                             <li class="nav-item ">
                                 <a class="nav-link" href="{{ route('socios.index') }}">List
