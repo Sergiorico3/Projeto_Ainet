63b357e43587dda4ff1e5802cfd0ded0a4f47a45
Pedro Santos <7psantos@gmail.com>
2019-05-30 18:23:23 +0100

    asd

--- resources/views/users/mostrarcertificado.blade.php
+++ resources/views/users/mostrarcertificado.blade.php
@@ -6,6 +6,7 @@
                 <div class="card-body">
                     <h5 class="card-title text-center">Certificado</h5>
                         <embed src="{{Storage::disk('public')->url('/docs_piloto').'/certificado_'.Auth::id().'.pdf'}}" width="800px" height="1000px" />
+                        
                 </div>
             </div>
         </div>
