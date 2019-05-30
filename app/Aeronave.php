e6221a61c69d46dd00642d3a5b3f92a5adb18ef8
sergiorico3 <clubofsk8ers@hotmail.com>
2019-05-30 13:24:35 +0100

    testes e softdeletes

--- app/Aeronave.php
+++ app/Aeronave.php
@@ -3,9 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Aeronave extends Model
 {
+    use SoftDeletes;
     protected $primaryKey = 'matricula';
     public $incrementing = false;    
 
